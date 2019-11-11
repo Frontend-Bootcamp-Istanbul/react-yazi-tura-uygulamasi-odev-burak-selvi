d85a16401364ee6cfa4e44cd3a3f99b5b8882f88
Burak Selvi <burakselvi@hotmail.com>
2019-11-11 19:00:05 +0300

    yazi ve tura rastgele olacak sekilde yapıldı ve sayılar dinamik bir şekilde oluşturuldu

--- src/CoinFlipper.js
+++ src/CoinFlipper.js
@@ -3,36 +3,59 @@ import Coin from './Coin';
 import './CoinFlipper.css';
 
 class CoinFlipper extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     // State üzerinde paranın başlangıçtaki durumunu veriyoruz, başlangıçta "tura" olsun.
     // Daha sonra şu anda paranın dönüp dönmeme durumunu da veriyoruz, başlangıçta para atılmamış olduğundan "false" olarak verdik.
     this.state = {
-        side: "tura",
-        donuyor: false
+      side: "tura",
+      donuyor: false,
+      turaCount: 0,
+      yaziCount: 0,
+      totalCount: 0,
+      buttonState: false
     }
   }
   handleClick = () => {
+    this.handleSide();
     // "At!" butonuna tıkladığımızda paranın dönmesini istiyoruz, bu yüzden "donuyor" durumunu "true" yapıyoruz.
-    this.setState({donuyor: true});
+    this.setState({ donuyor: true, buttonState: true, totalCount: this.state.totalCount + 1 });
     // 1 saniye kadar dönmesi yeterli, bu yüzden 1 saniye sonra "donuyor" durmunu tekrar "false" yapıyoruz.
-    setTimeout(() => this.setState({donuyor: false}), 1000);
+    setTimeout(() => this.setState({ donuyor: false, buttonState: false }), 1000);
+  };
+  handleSide = () => {
+    const sayi = Math.round(Math.random());
+    const sides = ['tura', 'yazi'];
+    const side = sides[sayi];
+    if (side === 'tura') {
+      this.setState({
+        side
+      }, () => {
+        setTimeout(() => this.setState({ turaCount: this.state.turaCount + 1 }), 1000);
+      });
+    } else {
+      this.setState({
+        side
+      }, () => {
+        setTimeout(() => this.setState({ yaziCount: this.state.yaziCount + 1 }), 1000);
+      });
+    }
   };
 
-  render(){
+  render() {
     return (
       <div className="CoinFlipper">
         <h1>Yazı mı Tura mı?</h1>
         <Coin side={this.state.side} donuyor={this.state.donuyor} />
-        <button onClick={this.handleClick} >At!</button>
+        <button onClick={this.handleClick} disabled={this.state.buttonState} >At!</button>
         <p>
-            Toplam
-            <strong> 5 </strong>
-            atıştan
-            <strong> 3 </strong>
-            ü tura
-            <strong> 2 </strong>
-            si yazı geldi.</p>
+          Toplam
+            <strong> {this.state.totalCount} </strong>
+          atıştan
+            <strong> {this.state.turaCount} </strong>
+          ü tura
+            <strong> {this.state.yaziCount} </strong>
+          si yazı geldi.</p>
       </div>
     )
   }
